f0c7e30c4dce63972255fffa9a7102cc3350aeb4
Desmond Chin <chin.sy.des@gmail.com>
2020-02-14 23:49:22 +0800

    add rocket and thrust

---
+++ Assets/Scenes/Rocket.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidbody.AddRelativeForce(Vector3.up * Time.deltaTime * 1000);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(-Vector3.forward * Time.deltaTime * 100);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * 100);
+        }
+    }
+}
