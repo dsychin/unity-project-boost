65527ebbbf440cc917b59d831649cae44e80f917
Desmond Chin <chin.sy.des@gmail.com>
2020-02-15 17:43:12 +0800

    add obstacles

--- Assets/Scenes/Rocket.cs
+++ Assets/Scenes/Rocket.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField]
+    float rcsThrust = 100f;
+
+    [SerializeField]
+    float mainThrust = 1000f;
+
     new Rigidbody rigidbody;
     AudioSource audioSource;
 
@@ -21,18 +27,32 @@ public class Rocket : MonoBehaviour
         Rotate();
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+            default:
+                print("dead");
+                break;
+        }
+    }
+
     private void Rotate()
     {
         rigidbody.freezeRotation = true;
+        var rotation = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Rotate(-Vector3.forward * Time.deltaTime * 100);
+            transform.Rotate(-Vector3.forward * rotation);
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Rotate(Vector3.forward * Time.deltaTime * 100);
+            transform.Rotate(Vector3.forward * rotation);
         }
 
         rigidbody.freezeRotation = false;
@@ -42,7 +62,7 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            rigidbody.AddRelativeForce(Vector3.up * Time.deltaTime * 1000);
+            rigidbody.AddRelativeForce(Vector3.up * Time.deltaTime * mainThrust);
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
